8184ea925428deac1f0eb9f8e5fe80d32f97a449
alex19992020 <alejandroantunez1999@gmail.com>
2024-01-23 17:32:14 -0800

    created some arrays

--- variablePractice.js
+++ variablePractice.js
@@ -4,4 +4,17 @@ const yourSecondVariable = 10;
 
 const yourThirdVariable = { firstProperty: "hello world" };
 
-console.log(yourVariable);
+const firstArray = [10, 234, 543, 213];
+
+const secondArray = [
+  4324,
+  12,
+  124,
+  1,
+  "a string",
+  { prop: "sdgsdfg" },
+  [43, 231],
+];
+
+console.log(firstArray[0]);
+console.log(secondArray[3][0]);
