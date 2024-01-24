724d07b2717b431dd34e47e250e46b5307830ace
alex19992020 <alejandroantunez1999@gmail.com>
2024-01-23 16:55:01 -0800

    writing variables

---
+++ variablePractice.js
@@ -0,0 +1,7 @@
+const yourVariable = "Learning to code gives you superpowers";
+
+const yourSecondVariable = 10;
+
+const yourThirdVariable = { firstProperty: "hello world" };
+
+console.log(yourVariable);
